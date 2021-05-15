a74444948fb1747482eaa75bf3ab6ec268b32c17
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-05-15 18:49:27 +0200

    Dismiss keyboard

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, View, FlatList, Alert } from 'react-native';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import Header from './components/header'
 import TodoItem from './components/todoItem'
@@ -37,21 +37,25 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Header/>
-      <View style = {styles.content}>
-        <AddTodo submitHandler={submitHandler}/>
-        <View style = {styles.list}>
-          <FlatList
-            data = {todos}
-            renderItem = {({item}) => (
-              <TodoItem item={item} pressHandler={pressHandler}/>
-            )}
-          />
+    <TouchableWithoutFeedback onPress = {() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard');
+    }}>
+      <View style={styles.container}>
+        <Header/>
+        <View style = {styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style = {styles.list}>
+            <FlatList
+              data = {todos}
+              renderItem = {({item}) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              )}
+            />
+          </View>
         </View>
       </View>
-      
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
