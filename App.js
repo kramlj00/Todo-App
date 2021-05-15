a42706e83794f901d018aa23f9405811b45e0c1a
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-05-15 18:17:57 +0200

    Adding new todo text input

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import Header from './components/header'
+import TodoItem from './components/todoItem'
+import AddTodo from './components/addTodo'
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ])
+
+  //funkcija pressHandler će se proslijedit kao prop u TodoItem komponentu
+  //klikom na neki todo brišemo ga
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todos => todos.key != key); /*prevTodos sadrži sve todos osim onog s tenutnim key*/
+    })
+  } 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <View style = {styles.content}>
+        <AddTodo/>
+        <View style = {styles.list}>
+          <FlatList
+            data = {todos}
+            renderItem = {({item}) => (
+              <TodoItem item={item} pressHandler={pressHandler}/>
+            )}
+          />
+        </View>
+      </View>
+      
     </View>
   );
 }
@@ -15,7 +43,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
