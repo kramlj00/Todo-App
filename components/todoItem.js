db2473cdec8d8365db50d54c424f21b55e5eb101
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-05-15 19:25:19 +0200

    Adding icons

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function TodoItems({ item, pressHandler }) {
     return (
         <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style = {styles.item}>{item.text}</Text>
+            <View style = {styles.item}>
+                <MaterialIcons name="delete" size={20} color="#333" />
+                <Text style = {styles.itemText}>{item.text}</Text>
+            </View>
         </TouchableOpacity>
     )
 }
@@ -17,5 +22,11 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderStyle: 'dashed',
         borderRadius: 10,
+        flex: 1,
+        flexDirection: 'row',
+    },
+    itemText: {
+        marginLeft: 10,
+        marginRight: 15,
     }
 })
